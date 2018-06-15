cad7576801fd698e55bc383fda97e8fad919e3f9
Gabriel Diez <diez.gabriel64@gmail.com>
2018-06-15 13:53:56 +0200

    implement build notification and push notification

--- main.go
+++ main.go
@@ -3,12 +3,29 @@ package main
 import (
 	"github.com/deckarep/gosx-notifier"
 	"log"
+	"time"
 )
 
 func main() {
-	n := gosxnotifier.NewNotification("Test notification")
+	n := buildNotification()
+	for {
+		time.Sleep(60 * time.Minute)
+		go pushNotification(&n)
+	}
+}
 
+func pushNotification (n* gosxnotifier.Notification) {
 	if err := n.Push(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func buildNotification () gosxnotifier.Notification {
+	n := gosxnotifier.NewNotification("It's break time ! Don't forget to drink and stretch")
+	n.Title = "Pause Reminder"
+	n.Sender = "com.apple.Stickies"
+	n.AppIcon = "gopher.png"
+	n.Sound = gosxnotifier.Default
+
+	return *n
 }
